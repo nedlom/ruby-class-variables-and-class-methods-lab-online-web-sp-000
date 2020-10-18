933fa4c8b0d445920205cab5a9f24fdc5e4fa475
imperative-scientist-4600 <nelom3@gmail.com>
2020-10-18 02:28:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,63 +43,7 @@ class Song
     end
     hash
   end
-  
 end
-
-
-
-
-
-
-
-
-
-  
-#   attr_accessor :name, :artist, :genre
-  
-#   @@count = 0
-#   @@artists = []
-#   @@genres = []
-  
-#   def initialize(name, artist, genre)
-#     @name = name
-#     @artist = artist
-#     @genre = genre
-#     @@count += 1
-#     @@artists << artist
-#     @@genres << genre
-#   end
-  
-#   def self.count
-#     @@count
-#   end
-
-#   def self.artists
-#     @@artists.uniq
-#   end
-  
-#   def self.genres
-#     @@genres.uniq
-#   end
-  
-#   def self.genre_count
-    
-#     Hash[genres.map{|genre| [genre, @@genres.count(genre)]}]
-#     # hash = {}
-#     # self.genres.each do |genre|
-#     #   hash[genre] = @@genres.count(genre)
-#     # end
-#     # hash
-#   end
-  
-#   def self.artist_count
-#     hash = {}
-#     self.artists.each do |artist|
-#       hash[artist] = @@artists.count(artist)
-#     end
-#     hash
-#   end
-  
 
 #   @@count = 0
 #   @@genres = []
